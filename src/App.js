a89f5e55b15d1874b72ba238f4706d5bfcfcecc5
Dhruv Prajapati <60540925+imdhruv99@users.noreply.github.com>
2024-08-03 10:35:27 +0530

    [ADDED] Post Idea (#1)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Common/Navbar';
 import LandingPage from './Components/Screens/LandingPage';
+import PostIdeaPage from './Components/Screens/PostIdeaPage';
 import Footer from './Components/Common/Footer';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<LandingPage />} />
+                    <Route path="/post-idea" element={<PostIdeaPage />} />
                 </Routes>
                 <Footer />
             </div>
--- src/Components/Common/Navbar.js
+++ src/Components/Common/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">IdeaBeacon</div>
-      <div className="navbar-login">
+      <div className="navbar-links">
+        <Link to="/">Home</Link>
+        <Link to="/post-idea">Post Idea</Link>
         <button className="login-button">Login</button>
       </div>
     </nav>
