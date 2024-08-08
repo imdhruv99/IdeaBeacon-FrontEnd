10812b6e64077d748a3881fbb5714227f285f3dc
imdhruv99 <dpdhruvprajapati@gmail.com>
2024-08-08 10:16:20 +0530

    [ADDED] new design for idea page

--- src/Components/Screens/ideaDetail/index.js
+++ src/Components/Screens/ideaDetail/index.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from "react";
 import { Button, TextField } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import ShareIcon from '@mui/icons-material/Share';
+import EditIcon from '@mui/icons-material/Edit';
 
 import { getIdeaDetail } from "../../Redux/api/ideaAPI";
 import moment from "moment-timezone";
@@ -29,99 +31,88 @@ const IdeaDetailsPage = () => {
   ) : (
     <div className="idea-details-page">
       <div className="idea-header">
-        <Button variant="contained" color="primary">
-          Share Idea
-        </Button>
-        <Button variant="contained" color="secondary">
-          Update Idea
-        </Button>
+        <div className="icon-container">
+          <ShareIcon className="icon-share" />
+          <EditIcon className="icon-edit" />
+        </div>
       </div>
       <div className="idea-content">
-        <p>
-        <strong>Idea Synopsis: </strong> {idea?.title}
-        </p>
-        <p>
-          <strong>Author:</strong> {idea?.createdBy.name}
-        </p>
-        <p>
-          <strong>Date Created:</strong> {moment(idea?.createdAt).format("YYYY-MM-DD")}
-        </p>
-        <p>
-          <strong>Stage:</strong> {idea?.ideaStageId.stageName}
-        </p>
-        <p>
-          <strong>Category:</strong> {idea?.ideaCategoryId.categoryName}
-        </p>
-        <p>
-          <strong>Presentable Date:</strong> {moment(idea?.presentableDate).format("YYYY-MM-DD")}
-        </p>
-        <p>
-          <strong>Private Idea:</strong> {idea?.isPrivate ? "Yes" : "No"}
-        </p>
-        <p>
-          <strong>Published:</strong> {idea?.published ? "Yes" : "No"}
-        </p>
-        <h3>Details</h3>
-        <p>
-          <strong>Problem Statement:</strong> <div dangerouslySetInnerHTML={{ __html: idea?.problemStatement }} />
-        </p>
-        <p>
-          <strong>Advantages:</strong> <div dangerouslySetInnerHTML={{ __html: idea?.advantage }} />
-        </p>
-        <p>
+        <div className="idea-synopsis">
+          {idea?.title}
+        </div>
+        <div className="idea-details-grid">
+      <div className="grid-item">
+        <div><strong>Author:</strong> {idea?.createdBy.name}</div>
+        <div><strong>Stage:</strong> {idea?.ideaStageId.stageName}</div>
+        <div><strong>Category:</strong> {idea?.ideaCategoryId.categoryName}</div>
+      </div>
+      <div className="grid-item">
+        <div><strong>Date Created:</strong> {moment(idea?.createdAt).format("DD-MM-YYYY")}</div>
+        <div><strong>Presentable Date:</strong> {moment(idea?.presentableDate).format("DD-MM-YYYY")}</div>
+        <div><strong>Private Idea:</strong> {idea?.isPrivate ? "Yes" : "No"}</div>
+        <div><strong>Published:</strong> {idea?.published ? "Yes" : "No"}</div>
+      </div>
+      <div className="grid-item">
+        <div><strong>Tags:</strong> {idea?.tags.join(", ")}</div>
+        <div><strong>Co-Authors:</strong> {idea?.coauthors.map((coauthor) => coauthor.name).join(", ")}</div>
+        <div><strong>Functions:</strong> {idea?.functionId.functionName}</div>
+        <div><strong>Sub Divisions:</strong> {idea?.subdivisionId.subdivisionName}</div>
+      </div>
+      </div>
+      <div className="details-section">
+        <div className="details-box">
+          <strong>Problem Statement:</strong>
+          <div dangerouslySetInnerHTML={{ __html: idea?.problemStatement }} />
+        </div>
+        <div className="details-box">
+          <strong>Advantages:</strong>
+          <div dangerouslySetInnerHTML={{ __html: idea?.advantage }} />
+        </div>
+        <div className="details-box">
           <strong>Proposed Solution:</strong>
           <div dangerouslySetInnerHTML={{ __html: idea?.proposedSolution }} />
-        </p>
-        <p>
-          <strong>Existing Solution:</strong> <div dangerouslySetInnerHTML={{ __html: idea?.existingSolution }} />
-        </p>
-        <p>
-          <strong>Tags:</strong> {idea?.tags.join(", ")}
-        </p>
-        <p>
-          <strong>Co-Authors:</strong> {idea?.coauthors.map((coauthor) => coauthor.name).join(", ")}
-        </p>
-        <p>
-          <strong>Functions:</strong> {idea?.functionId.functionName}
-        </p>
-        <p>
-          <strong>Sub Divisions:</strong> {idea?.subdivisionId.subdivisionName}
-        </p>
-        <h3>Idea Track Status</h3>
-        <table className="track-status-table">
-          <thead>
-            <tr>
-              <th>Event</th>
-              <th>Details</th>
-              <th>Timestamps</th>
-            </tr>
-          </thead>
-          <tbody>
-            {ideaAuditLogData?.map((event, index) => (
-              <tr key={index}>
-                <td>{event.eventName}</td>
-                <td>{event.details}</td>
-                <td>{moment(event.createdAt).format("YYYY-MM-DD")}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        </div>
+        <div className="details-box">
+          <strong>Existing Solution:</strong>
+          <div dangerouslySetInnerHTML={{ __html: idea?.existingSolution }} />
+        </div>
+      </div>
+      <div className="track-status-section">
+      <h3 className="track-status-title">Idea Tracking Progress</h3>
+      <div className="track-status-container">
+          <div className="track-status-header">
+            <div className="track-status-cell">Event</div>
+            <div className="track-status-cell">Details</div>
+            <div className="track-status-cell">Timestamps</div>
+          </div>
+            <div className="track-status-body">
+              {ideaAuditLogData?.map((event, index) => (
+                <div className="track-status-row" key={index}>
+                  <div className="track-status-cell">{event.eventName}</div>
+                  <div className="track-status-cell">{event.details}</div>
+                  <div className="track-status-cell">{moment(event.createdAt).format("DD-MM-YYYY hh:mm:ss A")}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="comments-separator"></div> {/* Horizontal line */}
         <h3>Comments</h3>
         <div className="comments-section">
           {idea?.comments?.map((comment, index) => (
             <div key={index} className="comment">
-              <p>
+              <div className="comment-text">
                 <strong>{comment.author}:</strong> {comment.text}
-              </p>
-              <p>
+              </div>
+              <div className="comment-date">
                 <em>{comment.date}</em>
-              </p>
+              </div>
             </div>
           ))}
           <div className="comment-input">
-            <TextField label="Add a Comment" variant="outlined" multiline rows={4} fullWidth />
+            <TextField className="comment-textfield" label="Add a Comment" variant="outlined" multiline rows={2} />
             <Button variant="contained" color="primary">
-              Submit
+              Comment
             </Button>
           </div>
         </div>
